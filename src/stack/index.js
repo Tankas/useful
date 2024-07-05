36dda231fd6c9fbb61954072724775f70f995abe
tanka <>
2024-07-05 17:51:10 +0800

    fix: --task=1 stack sort

--- src/stack/index.js
+++ src/stack/index.js
@@ -9,6 +9,10 @@ class Stack {
     this.minList = []
   }
 
+  getLen() {
+    return this.list.length
+  }
+
   push(item) {
     this.list.push(item)
     if (this.minList.length === 0) {
@@ -21,6 +25,32 @@ class Stack {
     }
   }
 
+
+  getLast(list) {
+
+    let item = list.pop();
+
+    if (list.length === 0) {
+      return item
+    } else {
+      const last = this.getLast(list)
+      list.push(item)
+      return last
+    }
+
+  }
+
+  reverise () {
+    if (this.list.length === 0) {
+      return
+    }
+    const last = this.getLast(this.list)
+    console.log('before', last)
+    this.reverise()
+    console.log('after', last)
+    this.list.push(last)
+    return last
+  }
 
   pop() {
     const item = this.list.pop()
@@ -36,16 +66,70 @@ class Stack {
   }
 
 }
+// 两个栈合成队列
+// 编写一个类，用两个栈实现队列，支持队列的基本操作（add、poll、peek）
 
-const a = new Stack()
-
-a.push(3)
-a.push(2)
-a.push(1)
-a.push(4)
-a.push(5)
-
-console.log(a)
-// console.log(a.getMin())
+class Queue {
+  constructor() {
+    this.stack1 = new Stack();
+    this.stack2 = new Stack();
+    this.size = 0;
+  }
 
 
+  add(item) {
+    this.stack1.push(item)
+    this.size ++
+  }
+
+  poll() {
+    for (let i = 0; i < this.size; i++) {
+      const item = this.stack1.pop();
+      this.stack2.push(item)
+    }
+    const item  = this.stack2.pop()
+    let temp = this.stack1;
+    this.stack1 = this.stack2;
+    this.stack2 = temp
+    return item
+  }
+
+}
+
+// 实现栈中元素的逆序，但是只能用递归函数来实现，不能用其他数据结构。
+// [1,2,3,4,5]
+// [5,4,3,2,1]
+
+
+
+const stack = new Stack();
+
+stack.push(4)
+stack.push(2)
+stack.push(1)
+stack.push(3)
+stack.push(5)
+
+
+
+// 用一个栈实现另一个栈的排序
+// 一个栈中元素的类型为整型，现在想将该栈从顶到底按从大到小的顺序排序，只许申请一个栈。 
+
+const sortStack = (stack) => {
+  let help = [];
+
+  while (stack.length > 0) {
+    const cur = stack.pop();
+    const top = help.pop()
+    // 如果当前元素比栈顶元素小，那么直接入栈
+    if (top === undefined) {
+      help.push(cur)
+    } else if (cur < top) {
+      help.push(top)
+      help.push(cur)
+    } else {
+      stack.push(top)
+      stack.push(cur)
+    }
+  }
+}
